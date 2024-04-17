05b037e13af7931baa73783c60a1bd5bf25e792f
Henri Sippola <hemsip@utu.fi>
2024-04-17 11:53:37 +0300

    add exercise 4-2 implementations

--- index.js
+++ index.js
@@ -1,43 +1,8 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+//const http = require('http')
+const { info, error } = require('./utils/logger')
+const config = require('./utils/config')
+const app = require('./app')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb+srv://henrisippola:<password>@cluster0.dfreifj.mongodb.net/blogApp?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  info(`Server running on port ${config.PORT}`)
 })
