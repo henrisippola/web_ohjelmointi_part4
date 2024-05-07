5ed1ac0b2db7cccc3866b22cda6ba396d70801ab
Henri Sippola <hemsip@utu.fi>
2024-05-07 22:21:24 +0300

    exercise5-1 implementations

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+      return authorization.replace('Bearer ', '')
+    }
+    return null
+  }
 
 blogsRouter.get('/', (request, response) => {
     Blog
